2fe13c175b89198ca6a74f4cc0450f763717c67e
adrie <adrie@10.65.13.91>
2022-08-25 12:00:59 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Adrien Palomo
+ * Email: apalomo3@huskers.unl.edu
+ * Date: 2022/08/25
+ * 
+ * A program to print my name
+ * 
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adrien Palomo\n");
+	}
+
+}
